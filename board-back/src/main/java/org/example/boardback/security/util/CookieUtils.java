2bae4437bae2ddfd73d489931179ade25d8e5dda
Kitbomin <zzxxcc1433@naver.com>
2025-11-25 10:17:50 +0900

    OAuth2 AthenticationHandler

---
+++ board-back/src/main/java/org/example/boardback/security/util/CookieUtils.java
@@ -0,0 +1,18 @@
+package org.example.boardback.security.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtils {
+    public static void addHttpOnlyCookie(
+            HttpServletResponse response, String name,
+            String value, int maxAge, boolean secure
+    ) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(secure);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+}
